90855b6f5a9fb391797f4363d106c9d5af6b1d00
Segun Osiki <osiki.segun@gmail.com>
2023-08-03 05:23:01 +0100

    first commit

---
+++ src/main/java/com/osiki/mortuarydemo/MortuaryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.osiki.mortuarydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortuaryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortuaryDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osiki/mortuarydemo/MortuaryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osiki.mortuarydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortuaryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
